d74bf2af9231eb757f128705a312cd87748d140c
David <dkdewitt@gmail.com>
2015-06-24 22:16:25 -0700

    Added Initial File Struct and Processor

--- source/app.d
+++ source/app.d
@@ -1,6 +1,64 @@
-import std.stdio;
+//import elastic_api;
+import std.range, std.stdio, std.file;
+import std.algorithm;
+import std.concurrency;
+import std.parallelism;
+import std.csv;
+import headers;
+import filestore;
+import std.array,std.conv;
 
-void main()
-{
-	writeln("Edit source/app.d to start your project.");
+
+
+void main() {
+
+
+
+    string path = "/home/david/cdrs/";
+    string[] files = listdir(path);
+    string[] fileDates = getFileDates(files);
+
+    //readfiles(path);
+    headers h1;
+    foreach(file; files){
+        writeln(file);
+        auto f1 = f_item!string();    
+        File f = File(path~file);
+        f1.readFile(f);
+        //headers h1;
+        auto hash1 = file_hash!string();
+        if (f1.data.length < 4){
+            continue;
+        }
+        f1.sliceData(3, f1.data.length-1);
+        hash1.hash_data(h1, f1.data);
+
+    }
 }
+
+
+
+private string[] listdir(string pathname)
+{
+
+    return std.file.dirEntries(pathname, SpanMode.shallow)
+        .filter!(a => a.isFile)
+        .filter!(a=> endsWith(a.name, ".csv"))
+        .map!(a => std.path.baseName(a.name))
+        .array;
+}
+
+string[] getFileDates(string[] files){
+    
+    string[][string] datemap;  
+    string[] dates;
+    foreach(string file; files){
+        auto split = splitter(file, "-").array;
+            dates ~= split[2][0..8];
+    }
+        dates = dates.uniq.array;
+        //dates.each!(a=>datemap[a] = string[]);
+        writeln(dates);
+    return dates;
+}
+
---
+++ source/filestore.d
@@ -0,0 +1,78 @@
+
+module filestore;
+import std.stdio;
+import std.range, std.algorithm, std.array;
+import headers;
+
+
+struct FileStore
+{
+    string[] fileNames;
+
+    this(string[] fileNames){
+        fileNames=fileNames;
+    }
+
+    void addFile(string fileName){
+        fileNames~=fileName;
+    }
+
+
+}
+
+struct f_item(T){
+    
+    T[] data;
+    void readFile(File dataIn){
+        char[] tempBuf;
+        import std.string, std.conv;
+        foreach (ubyte[] buffer; chunks(dataIn, 8092))
+        {
+
+            char[] x = cast(char[]) buffer;
+            tempBuf ~= x;
+            
+        }
+        string[] lines = splitLines(to!string(tempBuf));
+            
+        data = data ~= lines;
+    }
+
+    void insertData(T[] newData){
+       data =  data ~ newData;
+    }
+
+    void sliceData( size_t begin, size_t end){
+        data = data[begin..end];
+    }
+
+    T opIndex(in size_t t1)
+    {
+        return data[t1];
+    }
+
+
+}
+
+
+struct file_hash(T)
+{
+    T[T][] d;
+
+    void hash_data(headers h1,string[] data){
+
+        foreach(line; data){
+            auto lineArray = splitter(line, ",").array;
+            auto dict = zip(h1.fullHeader,lineArray).
+                filter!(a=>h1.filteredHeader.canFind(a[0])).assocArray;
+           
+            d ~= dict;
+        }
+    }
+}
+
+
+
+
+
+
